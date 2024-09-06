52463369ca6ac78a53c4e62a819bdb5fd40330bf
tunelessly <9685607+tunelessly@users.noreply.github.com>
2024-09-06 03:17:23 +0100

    Initial commit

---
+++ src/test/java/me/besunta/eveMapCompanion/EveMapCompanionTests.java
@@ -0,0 +1,13 @@
+package me.besunta.eveMapCompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveMapCompanionTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
